e957ea7dfad42bcdce862429d1e70b56493b9d88
askadavetli <askadavetliapp@gmail.com>
2026-08-28 03:34:23 +0300

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "../globals.css";
+
+export const metadata = {
+  title: "Askadavetli",
+  description: "Hayalindeki daveti birlikte tasarlayalım.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  );
+}
